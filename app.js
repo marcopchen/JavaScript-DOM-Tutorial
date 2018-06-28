7cc5398c4ebf0e7a29208df3608510f156b51519
Marco Poetry Chen <marcopchen@gmail.com>
2018-06-28 19:12:58 -0400

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const bookList = document.querySelector('#book-list ul');
 const { forms } = document;
 const addBook = forms['add-book'];
-const hideBox = addBook.querySelector('#hide');
+const searchBooks = forms['search-books'];
 
 bookList.addEventListener('click', (event) => {
   if (event.target.classList.contains('delete')) {
@@ -24,10 +24,47 @@ addBook.addEventListener('submit', (event) => {
   bookList.appendChild(book);
 });
 
+const hideBox = addBook.querySelector('#hide');
 hideBox.addEventListener('change', () => {
   if (hideBox.checked) {
     bookList.style.display = 'none';
   } else {
-    bookList.style.display = 'initial';
+    bookList.style.display = 'block';
+  }
+});
+
+const searchBox = searchBooks.querySelector('input');
+const books = bookList.querySelectorAll('li');
+searchBox.addEventListener('keyup', (event) => {
+  const term = event.target.value.toLowerCase();
+  books.forEach(book => {
+    const name = book.querySelector('.name').textContent;
+    if (!name.toLowerCase().includes(term)) {
+      book.style.display = 'none';
+    } else {
+      book.style.display = 'block';
+    }
+  });
+});
+
+const tabs = document.querySelector('.tabs');
+const panels = document.getElementsByClassName('panel');
+tabs.addEventListener('click', (event) => {
+  if (event.target.tagName === 'LI') {
+    const selected = panels[event.target.textContent.toLowerCase()];
+    Array.from(panels).forEach(panel => {
+      if (panel === selected) {
+        panel.classList.add('active');
+      } else {
+        panel.classList.remove('active');
+      }
+    });
+    tabs.querySelectorAll('li').forEach(tab => {
+      if (tab === event.target) {
+        tab.classList.add('active');
+      } else {
+        tab.classList.remove('active');
+      }
+    });
   }
 });
